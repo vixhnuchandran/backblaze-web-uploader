6e2ba090c297218eee874051a1c92b6c746ce06b
vixhnuchandran <vixhnuchandran@gmail.com>
2024-06-25 12:21:09 +0530

    Refactor 12

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
-export default {
-  base: './',
-};
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
+})
