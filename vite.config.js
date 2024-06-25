c7713abf60cb44995ba6768677fe3235138778a7
vixhnuchandran <vixhnuchandran@gmail.com>
2024-06-25 12:17:59 +0530

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-import { defineConfig } from "vite"
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-      },
-    },
-  },
-})
+export default {
+  base: './',
+};
